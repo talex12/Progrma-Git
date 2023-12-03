9a4e0c410ebb0291187b37938c8dc8c288032ca0
talex12 <tonya.alguerac@ecci.edu.co>
2023-12-03 11:34:04 -0500

    Primer proyecto

---
+++ js/Ejemplo.js
@@ -0,0 +1,16 @@
+var nombre = "Tony";
+var apellido = "Alguera";
+var numero1 = 4;
+var frutas = ["naranjas","peras","manzanas"];
+var objetopersona = {
+    nombre: "Tony",
+    apellido: "Alguera",
+    direccion: "Mi casa"
+};
+
+function mensaje(){ 
+    alert(nombre)
+    alert(numero1)
+    alert(frutas)
+    alert("Este objeto tiene el siguiente nombre:" + objetopersona.nombre + " Y el apellido del objeto es:" + objetopersona.apellido)
+    }
